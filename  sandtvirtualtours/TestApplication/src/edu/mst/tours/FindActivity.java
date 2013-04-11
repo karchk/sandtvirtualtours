53225bc0b182940ebfad84c4457cd1ee832ebed5
julioz92@gmail.com <julioz92@gmail.com@45b7c2a5-93f3-a9e2-6f8b-8d0d25a5df36>
2013-04-11 16:58:18 +0000

    It is not a good practice to name methods or variables starting with an upper-case letter It is a good practice to name activities' layouts as <name>activity.xml, so, for example mainactivity.xml, findactivity.xml and so on. When defining ids, use the pattern @+<activityname>/<elementtype>_<functionalityassociated>, so for example, a button that relates to finding buildings in findactivity.xml would be named as @+findactivity/bt_findbuildings Spinners can have their items defined in xml (layouts) with the attribute "entries" that maps the spinner to a string-array Instantiate elements of activities' layouts only once during the execution of that activity, this means there is no need to call findViewById(...) more than one time for a same element, just make that reference global to that class

---  sandtvirtualtours/TestApplication/src/edu/mst/tours/FindActivity.java
+++  sandtvirtualtours/TestApplication/src/edu/mst/tours/FindActivity.java
@@ -1,64 +1,50 @@
 package edu.mst.tours;
 
 import android.app.Activity;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
 public class FindActivity extends Activity {
 
+	private Spinner sp_departmentselector;
+	private TextView tv_building;
+	private Button bt_selectbuilding;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_find);
+		setContentView(R.layout.findactivity);
 		
-		Spinner spinner = (Spinner) findViewById(R.id.depsel);
-		// Create an ArrayAdapter using the string array and a default spinner layout
-		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-		        R.array.departments, android.R.layout.simple_spinner_item);
-		// Specify the layout to use when the list of choices appears
-		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		// Apply the adapter to the spinner
-		spinner.setAdapter(adapter);
-		spinner.setOnItemSelectedListener(new OnItemSelectedListener() {  
-			  public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {  
-				// hide selection text  
-				// if you want you can change background here  
-				Resources r = getResources();
-				String[] adapter = r.getStringArray(R.array.building_options);
-				TextView tv = (TextView) findViewById(R.id.Building);
-				if(position == 0)
-				{
-					//cs
-					tv.setText(adapter[0]);
-				}
-				else if(position == 1 || position == 2)
-				{
-					//havener
-					tv.setText(adapter[1]);
-				}
-				else if(position > 2 && position < 5)
-				{
-					//toomey
-					tv.setText(adapter[4]);
-				}
-				else if(position == 5)
-				{
-					//library
-					tv.setText(adapter[2]);
-				}
-				else if(position >= 5)
-				{
-					//parker
-					tv.setText(adapter[3]);
-				}
-			  }  
-			  public void onNothingSelected(AdapterView<?> arg0) {}  
-			 });
+		loadViews();
+		
+		sp_departmentselector.setOnItemSelectedListener(new OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+				String[] adapter = getResources().getStringArray(R.array.building_options);
+				int indexToGet = 0;
+				if(position == 0) indexToGet = 0; //cs
+				else if(position == 1 || position == 2) indexToGet = 1; //havener
+				else if(position > 2 && position < 5) indexToGet = 4; //toomey
+				else if(position == 5) indexToGet = 2; //library
+				else if(position >= 5) indexToGet = 3; //parker
+				
+				tv_building.setText(adapter[indexToGet].toString());
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) { }
+		});
+	}
+
+	private void loadViews() {
+		sp_departmentselector = (Spinner) findViewById(R.findactivity.sp_departmentselector);
+		tv_building = (TextView) findViewById(R.findactivity.tv_building);
+		bt_selectbuilding = (Button) findViewById(R.findactivity.bt_selectbuilding);
 	}
 }
