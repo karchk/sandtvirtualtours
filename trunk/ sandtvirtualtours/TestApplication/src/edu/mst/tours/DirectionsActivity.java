0b0634360869c9737dfef54da00c7b4f14178cf5
julioz92@gmail.com <julioz92@gmail.com@45b7c2a5-93f3-a9e2-6f8b-8d0d25a5df36>
2013-04-11 16:58:18 +0000

    It is not a good practice to name methods or variables starting with an upper-case letter It is a good practice to name activities' layouts as <name>activity.xml, so, for example mainactivity.xml, findactivity.xml and so on. When defining ids, use the pattern @+<activityname>/<elementtype>_<functionalityassociated>, so for example, a button that relates to finding buildings in findactivity.xml would be named as @+findactivity/bt_findbuildings Spinners can have their items defined in xml (layouts) with the attribute "entries" that maps the spinner to a string-array Instantiate elements of activities' layouts only once during the execution of that activity, this means there is no need to call findViewById(...) more than one time for a same element, just make that reference global to that class

--- trunk/ sandtvirtualtours/TestApplication/src/edu/mst/tours/DirectionsActivity.java
+++ trunk/ sandtvirtualtours/TestApplication/src/edu/mst/tours/DirectionsActivity.java
@@ -2,72 +2,52 @@ package edu.mst.tours;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 public class DirectionsActivity extends Activity {
 
+	private final static String GOOGLEMAPS_URL_PREFIX = "http://maps.google.com/maps?saddr=";
+	private final static String GOOGLEMAPS_URL_TO_APPEND = "&daddr=";
+	
+	private Button bt_getdirections, bt_finddepartment;
+	private Spinner sp_from, sp_to;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_directions);
-
-		Intent intent = getIntent();
-		String message = intent.getStringExtra("Building");
-		if(message != null)
-		{
-			
-		}
-		Spinner spinner = (Spinner) findViewById(R.id.bldsel1);
-		// Create an ArrayAdapter using the string array and a default spinner layout
-		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-		        R.array.building_options, android.R.layout.simple_spinner_item);
-		// Specify the layout to use when the list of choices appears
-		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		// Apply the adapter to the spinner
-		spinner.setAdapter(adapter);
-		Spinner spinner2 = (Spinner) findViewById(R.id.bldsel2);
-		spinner2.setAdapter(adapter);
-		spinner.setOnItemSelectedListener(new OnItemSelectedListener() {  
-		  public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {  
-			// hide selection text  
-			((TextView)view).setText(null);  
-			// if you want you can change background here 
-			Resources r = getResources();
-			String[] adapter = r.getStringArray(R.array.building_locs);
-			EditText box1 = (EditText) findViewById(R.id.start_location);
-			box1.setText(adapter[position]);
-		  }  
-		  public void onNothingSelected(AdapterView<?> arg0) {}  
-		 });
-		spinner2.setOnItemSelectedListener(new OnItemSelectedListener() {  
-			  public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {  
-				// hide selection text  
-				((TextView)view).setText(null);  
-				// if you want you can change background here 
-				Resources r = getResources();
-				String[] adapter = r.getStringArray(R.array.building_locs);
-				EditText box1 = (EditText) findViewById(R.id.end_location);
-				box1.setText(adapter[position]);
-			  }  
-			  public void onNothingSelected(AdapterView<?> arg0) {}  
-			 });
+		setContentView(R.layout.directionsactivity);
+		
+		loadViews();
 	}
 	
-	public void QueryDirections(View view) {
-		// do nothing for now
-		EditText begBox = (EditText) findViewById(R.id.start_location);
-		EditText endBox = (EditText) findViewById(R.id.end_location);
-		Intent intent = new Intent(android.content.Intent.ACTION_VIEW, 
-			    Uri.parse("http://maps.google.com/maps?saddr=" + begBox.getText() + "&daddr=" + endBox.getText()));
-			startActivity(intent);
+	private void loadViews() {
+		bt_getdirections = (Button) findViewById(R.directionsactivity.bt_getdirections);
+		bt_finddepartment = (Button) findViewById(R.directionsactivity.bt_finddepartment);
+		sp_from = (Spinner) findViewById(R.directionsactivity.sp_from);
+		sp_to = (Spinner) findViewById(R.directionsactivity.sp_to);
+		
+		bt_getdirections.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String from = sp_from.getSelectedItem().toString();
+				String to = sp_to.getSelectedItem().toString();
+				Intent intent = new Intent(android.content.Intent.ACTION_VIEW, 
+					    Uri.parse(GOOGLEMAPS_URL_PREFIX + from + GOOGLEMAPS_URL_TO_APPEND + to));
+					startActivity(intent);
+			}
+		});
+		
+		bt_finddepartment.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(v.getContext(), FindActivity.class);
+		        startActivity(intent);
+			}
+		});
 	}
 }
--- trunk/ sandtvirtualtours/TestApplication/src/edu/mst/tours/MainActivity.java
+++ trunk/ sandtvirtualtours/TestApplication/src/edu/mst/tours/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.mainactivity);
         
         loadViews();
         
